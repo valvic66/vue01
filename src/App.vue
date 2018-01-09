cdd5f3936c674dc98685beb681ed04e4e5114073
M V <valvic6@gmail.com>
2018-01-09 20:25:18 +0200

    shopping cart in work

--- src/App.vue
+++ src/App.vue
@@ -12,34 +12,77 @@
     <break></break>
     <navigation-menu></navigation-menu>
     <break></break>
+    <div style="text-align: center">- IN WORK -</div>
+    <div class="product">
+      <product
+        v-for="(product, index) in products"
+        :key="index"
+        :product="product"
+      ></product>
+    </div>
+
+    <div class="shopping-cart">
+      <shopping-cart></shopping-cart>
+    </div>
+
   </div>
 </template>
 
 <script>
-import Break from './components/Break'
-import InstantSearch from './components/InstantSearch'
-import ToDo from './components/ToDo'
-import ConditionalServices from './components/ConditionalServices'
-import NavigationMenu from './components/NavigationMenu'
-import CounterParent from './components/CounterParent'
-
+import Break from "./components/Break";
+import InstantSearch from "./components/InstantSearch";
+import ToDo from "./components/ToDo";
+import ConditionalServices from "./components/ConditionalServices";
+import NavigationMenu from "./components/NavigationMenu";
+import CounterParent from "./components/CounterParent";
+import Product from "./components/Product";
+import ShoppingCart from "./components/ShoppingCart";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          title: "Product no. 1",
+          price: 300,
+          image: "apple_1.jpg",
+          qty: 1
+        },
+        {
+          id: 2,
+          title: "Product no. 2",
+          price: 500,
+          image: "apple_2.jpg",
+          qty: 1
+        },
+        {
+          id: 3,
+          title: "Product no. 3",
+          price: 100,
+          image: "apple_3.jpg",
+          qty: 1
+        }
+      ]
+    }
+  },
   components: {
     Break,
     InstantSearch,
     ToDo,
     NavigationMenu,
     CounterParent,
-    ConditionalServices
+    ConditionalServices,
+    Product,
+    ShoppingCart
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
@@ -48,10 +91,21 @@ export default {
 body {
   background-color: transparent;
 }
+
 </style>
 
 <style scoped>
-  .my-button {
-    width: 200px;
-  }
+.my-button {
+  width: 200px;
+}
+.product {
+  margin: 0 8px;
+
+  display: flex;
+}
+.shopping-cart {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
 </style>
